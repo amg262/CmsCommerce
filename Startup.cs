d94c545d02d4dc40550c62427811730259a7dc2e
Andrew Gunn <andrewgunn31@gmail.com>
2023-03-22 14:43:15 -0500

    WORKS!

--- Startup.cs
+++ Startup.cs
@@ -28,6 +28,7 @@ public class Startup
         services
             .AddCmsAspNetIdentity<ApplicationUser>()
             .AddCms()
+            .AddCommerce()
             .AddCmsTagHelpers()
             .AddAlloy()
             .AddAdminUserRegistration()
