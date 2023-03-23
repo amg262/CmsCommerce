adfa83c3f8984ebc456159aed3edc830545e2422
Andrew Gunn <andrewgunn31@gmail.com>
2023-03-23 11:18:55 -0500

    Added CmsCommerce model, controller, view for another custom page type

---
+++ Controllers/CmsCommercePageController.cs
@@ -0,0 +1,15 @@
+﻿using CmsCommerce.Models.Pages;
+using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CmsCommerce
+{
+    public class CmsCommercePageController : PageController<CmsCommercePage>
+    {
+        [HttpGet]
+        public IActionResult Index(CmsCommercePage currentPage)
+        {
+            return View(currentPage);
+        }
+    }
+}
---
+++ Models/Pages/CmsCommercePage.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CmsCommerce.Models.Pages
+{
+    [ContentType(
+        DisplayName = "CmsCommerce page",
+        GUID = "934E7266-FB8C-4DEA-B033-3B4E6AE6CBCF",
+        GroupName = Globals.GroupNames.Specialized,
+        Description = "Other page types",
+        AvailableInEditMode = true)]
+    [ImageUrl("~/styles/images/page_type.png")]
+    public class CmsCommercePage : SitePageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Title",
+            Description = "Title for the page",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual string Title { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "Main body",
+            GroupName = SystemTabNames.Content,
+            Order = 2)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
